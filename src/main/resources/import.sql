6ae73a1ab4971d44eafbd32fe82e86eeb372526b
Johann Bandion <Johann@bandion.net>
2023-04-21 17:00:59 +0200

    added test kafka endoint

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,1 @@
--- This file allow to write SQL commands that will be emitted in test and dev.
--- The commands are commented as their support depends of the database
--- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-1');
--- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-2');
--- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-3');
+insert into USERS (name, password) values ('Johann Bandion', 'password');
