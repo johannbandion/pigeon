0de1cf4510038379db0214feb9e6a172533ca35b
TobiasJ <jamtod19@htl-kaindorf.at>
2023-04-25 21:50:27 +0200

    Adding toolbar and textbubbels

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MessageComponent } from './message/message.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    MessageComponent,
   ],
   imports: [
     BrowserModule,
