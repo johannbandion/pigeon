7815bdd80650b9633226ab52af2ec33aba4ec429
Johann Bandion <Johann@bandion.net>
2023-06-02 12:34:12 +0200

    worked on login

--- src/main/java/at/kaindorf/persistence/repository/UserRepository.java
+++ src/main/java/at/kaindorf/persistence/repository/UserRepository.java
@@ -1,12 +1,12 @@
-//package at.kaindorf.persistence.repository;
-//
-//
-//import at.kaindorf.persistence.entity.UserEntity;
-//import io.quarkus.hibernate.orm.panache.PanacheRepository;
-//
-//import javax.enterprise.context.RequestScoped;
-//
-//@RequestScoped
-//public class UserRepository implements PanacheRepository<UserEntity> {
-//
-//}
+package at.kaindorf.persistence.repository;
+
+
+import at.kaindorf.persistence.entity.UserEntity;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import javax.enterprise.context.RequestScoped;
+
+@RequestScoped
+public class UserRepository implements PanacheRepository<UserEntity> {
+
+}
