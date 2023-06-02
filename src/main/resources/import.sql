ae618014c9d4e83eba47835ee6344daa563f9bef
Johann Bandion <Johann@bandion.net>
2023-06-02 12:41:15 +0200

    mod data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,12 @@
 -- insert into USERS (name, password) values ('Johann Bandion', 'password');
+
+insert into UserEntity (userName, password) values ('Johann Bandion', 'password');
+insert into UserEntity (userName, password) values ('Tobias Jamnik', 'password');
+insert into UserEntity (userName, password) values ('Tobias Bandion', 'password');
+insert into UserEntity (userName, password) values ('Johann Jamnik', 'password');
+insert into UserEntity (userName, password) values ('Lendard Bandion', 'password');
+insert into UserEntity (userName, password) values ('Johann Okron', 'password');
+insert into UserEntity (userName, password) values ('Lenard Okorn', 'password');
+insert into UserEntity (userName, password) values ('Lenard Jamnik', 'password');
+insert into UserEntity (userName, password) values ('Tobias Okorn', 'password');
+insert into UserEntity (userName, password) values ('Moritz Moritz', 'password');
