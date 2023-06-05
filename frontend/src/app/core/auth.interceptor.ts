fc25b017bdd80fb01a65a19192b67fb1b6a20f14
Johann Bandion <Johann@bandion.net>
2023-06-05 19:26:51 +0200

    auth guard and interceptor finished

--- frontend/src/app/core/auth.interceptor.ts
+++ frontend/src/app/core/auth.interceptor.ts
@@ -6,13 +6,22 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import {AuthService} from "../shared/auth.service";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const access_token: string | undefined  = this.authService.getDecodedAccessToken();
+
+    if (this.authService.isLoggedIn()) {
+      const clonedRequest = request.clone({
+        headers: request.headers.set("Authorization", "Bearer " + access_token)
+      })
+      return next.handle(clonedRequest);
+    }
     return next.handle(request);
   }
 }
